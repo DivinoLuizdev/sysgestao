d741988d8198c0763db666460c502be9a3b3d3b6
juniorwar360 <juniorwar360@gmail.com>
2022-10-18 21:07:55 -0300

    05

--- src/main/java/br/com/aulaws/sysgestao/repository/EnderecoRepository.java
+++ src/main/java/br/com/aulaws/sysgestao/repository/EnderecoRepository.java
@@ -3,10 +3,9 @@ package br.com.aulaws.sysgestao.repository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import br.com.aulaws.sysgestao.domain.Cliente;
 import br.com.aulaws.sysgestao.domain.Endereco;
 
 @Repository
-public class EnderecoRepository extends JpaRepository<Endereco, Long> {
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
 
 }
